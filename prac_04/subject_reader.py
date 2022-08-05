9cabc437f99e4fde4125c1ea0074d9b69c3a8a97
isaac <isaacwonghy@gmail.com>
2022-08-05 10:41:33 +0800

    Add subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,39 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    """Start of subject reader program."""
+    subjects = load_subjects()
+    display_subjects(subjects)
+    total_number_of_students = get_total_number_of_students(subjects)
+    print(f"Total number of students taking {len(subjects)} subjects = {total_number_of_students}")
 
 
-def get_data():
+def get_total_number_of_students(subjects):
+    '''Calculate the total number of students in all subjects.'''
+    total_students = 0
+    for subject in subjects:
+        total_students += subject[2]
+
+    return total_students
+
+
+def display_subjects(subjects):
+    """Display subject code, lecturer and number of students."""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:4} students")
+
+
+def load_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    subjects = []  # a list of lists(a list of subject list)
+    input_file = open(FILENAME, "r")
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        parts[2] = int(parts[2])  # Make the number an integer
+        subjects.append(parts)  # put in the subject into a list of subs
     input_file.close()
+    return subjects  # return list of lists
 
 
 main()
